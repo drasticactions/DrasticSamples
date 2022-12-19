db19ba2c13adc309ed9f35ed6631ef1007dba0c0
Timothy Miller <innerlogic4321@gmail.com>
2022-12-19 09:32:53 +0900

    Basic string generation

---
+++ SharedPlayground/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedPlayground
+{
+    public static class StringExtensions
+    {
+        public static string ToHtmlParagraph(this string str)
+            => $"<p>{str}</p>";
+
+        public static string GenerateHtmlString(this Faker faker, int paragraphs = 1)
+        {
+            var str = new StringBuilder();
+            paragraphs = paragraphs < 1 ? 1 : paragraphs;
+
+            var p = faker.Make(paragraphs, () => { return faker.Lorem.Paragraph(); });
+
+            var linkText = "<a href=\"" + faker.Internet.Url() + "\">" + faker.Lorem.Word() + "</a>";
+
+            foreach (var item in p)
+            {
+                var link = item.Insert(faker.Random.Int(0, item.Split(' ').Count() - 1), linkText);
+                str.Append(link.ToHtmlParagraph());
+            }
+
+            return str.ToString();
+        }
+    }
+}
